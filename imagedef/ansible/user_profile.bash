2a1ab6d41fe5dea1c8b51a91c3ca905a133880a3
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2018-02-11 00:17:08 +0000

    nicer? PS1

--- imagedef/ansible/user_profile.bash
+++ imagedef/ansible/user_profile.bash
@@ -3,7 +3,8 @@ RED="\[\e[1;31m\]"
 GREEN="\[\e[1;32m\]"
 MAGENTA="\[\e[1;35m\]"
 YELLOW="\[\e[1;33m\]"
+WHITE="\[\e[1;37m\]"
 
-export PS1="${GREEN}\h${NORMAL}@${MAGENTA}\w${YELLOW} ∆${NORMAL} "
+export PS1="${GREEN}$MACUSER${WHITE}@${MAGENTA}\h${NORMAL} \w${YELLOW} ∆${NORMAL} "
 
 
