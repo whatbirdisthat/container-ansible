9264fa7715e704e71aead8d23549f42807d57f44
What Bird Is That? <95787+whatbirdisthat@users.noreply.github.com>
2018-02-15 00:14:53 +0000

    use bash not ash, completion etc

--- imagedef/ansible/user_profile.bash
+++ imagedef/ansible/user_profile.bash
@@ -7,6 +7,7 @@ WHITE="\[\e[1;37m\]"
 
 export PS1="${GREEN}$MACUSER${WHITE}@${MAGENTA}\h${NORMAL} \w${YELLOW} ∆${NORMAL} "
 
+source /etc/profile.d/bash_completion.sh
+
 ANSIBLE_VERSION=`ansible --version | grep 'ansible' | head -n1 | awk ' { print $2 } '`
 echo -e "\e[1;33mContainer-Ansible : ${ANSIBLE_VERSION}\e[0m"
-
